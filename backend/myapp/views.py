0de93df2f0dbd0e00de894903e193459d83d0ac8
kaya <huaydin@student.42vienna.com>
2024-04-05 18:04:12 +0200

    a

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -27,44 +27,6 @@ token_obtain_pair_view = TokenObtainPairView.as_view()
 token_refresh_view = TokenRefreshView.as_view()
 
 @csrf_exempt
-def ChatPage(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        room = request.POST['room']
-
-        try:
-            get_room = Room.objects.get(room_name=room)
-            return redirect('room', room_name=room, username=username)
-
-        except Room.DoesNotExist:
-            new_room = Room(room_name = room)
-            new_room.save()
-            return redirect('room', room_name=room, username=username)
-
-    return render(request, 'chat.html')
-
-@csrf_exempt
-def MessageView(request, room_name, username):
-
-    get_room = Room.objects.get(room_name=room_name)
-
-    if request.method == 'POST':
-        message = request.POST['message']
-
-        print(message)
-
-        new_message = Message(room=get_room, sender=username, message=message)
-        new_message.save()
-
-    get_messages= Message.objects.filter(room=get_room)
-    
-    context = {
-        "messages": get_messages,
-        "user": username
-    }
-    return render(request, 'message.html', context)
-
-@csrf_exempt
 def signin42b(request):
     # Retrieve the redirect URI and client ID from environment variables
     redirect_uri = os.getenv('VANILLA_REDIRECT_URI')
