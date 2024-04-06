ca9216859caa3b7a20fc8dd0e873344cb0a7a7b1
kaya <huaydin@student.42vienna.com>
2024-04-06 22:55:10 +0200

    a

--- frontend/js/ViewProfile.js
+++ frontend/js/ViewProfile.js
@@ -1,23 +1,28 @@
 async function fetchAndDisplayViewProfile(username) {
     console.log(username);
     try {
-      const response = await fetch(`${getBackendURL()}/api/profiles/?username=${username}`);
-      if (!response.ok) {
-          throw new Error('Profile not found');
-      }
-      console.log('Response:', response);
+        const response = await fetch(`${getBackendURL()}/api/profiles/?username=${username}`);
+        if (!response.ok) {
+            throw new Error('Profile not found');
+        }
+
         const profileData = await response.json();
         console.log('Profile data:', profileData);
 
-      // Display profile data
-      document.querySelector('.profile-pic').src = profileData.image_link || '../src/emptyavatar.jpeg';
-      document.getElementById('nicknameadr').textContent = profileData.nickname || 'Not available';
-      document.getElementById('nicknameadr2').textContent = profileData.login || 'Not available';
-      document.getElementById('scoreadr').textContent = profileData.score || 'Not available';
+        // Accessing nested data under 'user'
+        const user = profileData.user || {}; // Default to an empty object if user data is missing
+        const imageLink = user.image_link || '../src/emptyavatar.jpeg';
+        const nickname = user.nickname || 'Not available';
+        const login = user.login || 'Not available';
+        const score = user.score || '0';
 
-      
-  } catch (error) {
-      console.error('Error fetching and displaying profile:', error);
-      // Handle error, e.g., display error message
-  }
+        // Display profile data
+        document.querySelector('.profile-pic').src = imageLink;
+        document.getElementById('nicknameadr').textContent = nickname;
+        document.getElementById('nicknameadr2').textContent = login;
+        document.getElementById('scoreadr').textContent = score;
+    } catch (error) {
+        console.error('Error fetching and displaying profile:', error);
+        // Handle error, e.g., display error message
+    }
 }
