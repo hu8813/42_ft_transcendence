7acb7a61172fc113906b527b824c413ff494ba5f
hu8813 <huaydin@student.42vienna.com>
2024-04-10 20:52:09 +0200

    a

--- frontend/js/Home.js
+++ frontend/js/Home.js
@@ -14,6 +14,45 @@ function showHome() {
     const pongTable = document.createElement("div");
     pongTable.classList.add("pong-table");
     
+    pongTable.onmouseover = () => {
+        const leftPaddle = document.querySelector(".paddle-left");
+        const rightPaddle = document.querySelector(".paddle-right");
+        const pongBall = document.querySelector(".ping-pong-ball");
+        const pongTable = document.querySelector(".pong-table");
+        
+        pongTable.onmousemove = (event) => {
+            const y = event.clientY - pongTable.offsetTop;
+            const leftPaddleTop = Math.min(400 - 100, Math.max(0, y - 50));
+            leftPaddle.style.top = leftPaddleTop + "px";
+            
+            const rightPaddleTop = Math.min(400 - 100, Math.max(0, y - 50));
+            rightPaddle.style.top = rightPaddleTop + "px";
+        };
+        
+        let ballTop = 200;
+        let ballLeft = 300;
+        let ballSpeedTop = 2;
+        let ballSpeedLeft = 2;
+        
+        const ballMove = () => {
+            ballTop += ballSpeedTop;
+            ballLeft += ballSpeedLeft;
+            
+            if (ballTop <= 0 || ballTop >= 400 - 2 * 2) {
+                ballSpeedTop = -ballSpeedTop;
+            }
+            
+            if (ballLeft <= 0 || ballLeft >= 600 - 2 * 2) {
+                ballSpeedLeft = -ballSpeedLeft;
+            }
+            
+            pongBall.style.top = ballTop + "px";
+            pongBall.style.left = ballLeft + "px";
+        };
+        
+        setInterval(ballMove, 10);
+    };
+    
     // ping-pong ball
     const pongBall = document.createElement("div");
     pongBall.classList.add("ping-pong-ball");
