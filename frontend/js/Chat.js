ff3297543859d4955bf77ed22eb0a238920fdd0e
kaya <huaydin@student.42vienna.com>
2024-04-11 19:30:32 +0200

    a

--- frontend/js/Chat.js
+++ frontend/js/Chat.js
@@ -159,13 +159,11 @@ function displayCachedMessages() {
     });
 
     function formatDate(dateString) {
-        console.log("date: "+dateString);
         const date = new Date(dateString); 
         const day = String(date.getDate()).padStart(2, '0');
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const hours = String(date.getHours()).padStart(2, '0');
         const minutes = String(date.getMinutes()).padStart(2, '0');
-        console.log("date: "+day+"/"+month+" "+hours+":"+minutes);
         return `${day}/${month} ${hours}:${minutes}`;
     }
 
