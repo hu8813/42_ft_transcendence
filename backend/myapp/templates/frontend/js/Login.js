04905615cf504aa390dba33742933ccaf4ab9ef3
hu8813 <huaydin@student.42vienna.com>
2024-04-15 21:35:54 +0200

    a

--- backend/myapp/templates/frontend/js/Login.js
+++ backend/myapp/templates/frontend/js/Login.js
@@ -12,29 +12,30 @@ function handleLogin() {
           const formData = new FormData(loginForm);
       
           try {
-              const response = await fetch(`${getBackendURL()}/login/`, {
-                  method: "POST",
-                  body: formData,
-              });
-                    
-              const data = await response.text();
-              
-              if (data.hasOwnProperty('message') && data.message === "Login successful") {
-                localStorage.setItem("isLoggedIn","true");
+            const response = await fetch(`${getBackendURL()}/login/`, {
+                method: "POST",
+                body: formData,
+            });
+        
+            const data = await response.json(); // Parse response as JSON
+        
+            if (data.hasOwnProperty('message') && data.message === "Login successful") {
+                localStorage.setItem("isLoggedIn", "true");
                 if (data.hasOwnProperty('jwtToken')) {
                     localStorage.setItem("jwtToken", data.jwtToken);
                 }
-                
+        
                 window.location.href = "/";
             } else {
                 console.error("Login failed:", data);
-                
+        
                 if (data.hasOwnProperty('error')) {
                     loginStatus.textContent = data.error;
                 } else {
                     loginStatus.textContent = "Login failed. Please check your credentials.";
                 }
             }
+        
         } catch (error) {
             console.error("Error logging in:", error);
             
