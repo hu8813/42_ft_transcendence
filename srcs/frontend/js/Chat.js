e0abd97bb67dd717b9d157ae69ff57fa4126decc
hu8813 <huaydin@student.42vienna.com>
2024-04-19 18:24:21 +0200

    a

--- srcs/frontend/js/Chat.js
+++ srcs/frontend/js/Chat.js
@@ -329,7 +329,7 @@ async function updateOnlineUsers() {
 
             // View Profile Link
             const viewProfileLink = document.createElement('a');
-            viewProfileLink.href = `/profile/${user.username}`;
+            viewProfileLink.href = `/#viewprofile?u=${user.username}`;
             viewProfileLink.classList.add('btn', 'btn-info', 'btn-sm', 'mr-1');
             viewProfileLink.innerHTML = '<i class="bi bi-search"></i> View Profile';
             viewProfileLink.target = '_blank'; // Open in new tab
@@ -337,7 +337,7 @@ async function updateOnlineUsers() {
 
             // Add Friend Link
             const addFriendLink = document.createElement('a');
-            addFriendLink.href = `/add-friend/${user.username}`;
+            addFriendLink.href = `/#add-friend?u=${user.username}`;
             addFriendLink.classList.add('btn', 'btn-success', 'btn-sm', 'mr-1');
             addFriendLink.innerHTML = '<i class="bi bi-plus"></i> Add Friend';
             addFriendLink.addEventListener('click', (event) => {
@@ -349,7 +349,7 @@ async function updateOnlineUsers() {
 
             // Block Link
             const blockLink = document.createElement('a');
-            blockLink.href = `/block/${user.username}`;
+            blockLink.href = `/#block/u=?${user.username}`;
             blockLink.classList.add('btn', 'btn-danger', 'btn-sm');
             blockLink.innerHTML = '<i class="bi bi-dash"></i> Block';
             blockLink.addEventListener('click', (event) => {
