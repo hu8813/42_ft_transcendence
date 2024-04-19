9cbe6d84d348f8eee223592afbf9c2486b7cb288
hu8813 <huaydin@student.42vienna.com>
2024-04-19 19:31:21 +0200

    a

--- srcs/frontend/js/Login.js
+++ srcs/frontend/js/Login.js
@@ -39,7 +39,8 @@ function handleLogin() {
                   if ('userLogin' in data) {
                       localStorage.setItem("userLogin", data.userLogin);
                   }
-
+                  
+                    
                   setTimeout(() => {
                       if (submitButton)
                           submitButton.disabled = false;
