92d2a3d5c67046bbc03cd825cfa3533922d86dc7
hu8813 <huaydin@student.42vienna.com>
2024-04-19 11:42:20 +0200

    a

--- srcs/frontend/js/Login.js
+++ srcs/frontend/js/Login.js
@@ -1,78 +1,78 @@
 function handleLogin() {
+  const loginForm = document.getElementById("login-form2");
+  const loginStatus = document.getElementById("login-status");
+  const submitButton = loginForm.querySelector('[type="submit"]');
 
-    const loginForm = document.getElementById("login-form2");
-    const loginStatus = document.getElementById("login-status");
-    const submitButton = loginForm.querySelector('[type="submit"]');
-  
-    if (loginForm) {
+  if (loginForm) {
       const handleSubmit = async (e) => {
-        e.preventDefault();
-  
-        const formData = new FormData(loginForm);
-        if (submitButton)
-            submitButton.disabled = true;
-        let csrfToken = await getCSRFCookie();
-        try {
-          const response = await fetch(`${getBackendURL()}/login/`, {
-            method: "POST",
-            body: formData,
-            headers: {
-              "X-CSRFToken": csrfToken,
-            },
-          });
-  
-        //   if (!response.ok) {
-        //     throw new Error('Failed to fetch login');
-        //   }
-  
-          const data = await response.json();
-  
-          if ('message' in data && data.message === "Login successful") {
-            localStorage.setItem("isLoggedIn", "true");
-            if ('jwt_token' in data) {
-              localStorage.setItem("jwtToken", data.jwt_token);
-            }
-            if ('userLogin' in data) {
-              localStorage.setItem("userLogin", data.userLogin);
-            }
-           
-  
-            setTimeout(() => {
-              if (submitButton)
-                submitButton.disabled = false;
-              window.location.href = "/";
-            }, 1000); 
-  
-          } else {
-  
-            if (data.hasOwnProperty('error')) {
-              loginStatus.textContent = data.error;
-              loginStatus.style.color = "red";
-            } else {
-              loginStatus.textContent = "Login failed. Please check your credentials.";
-              loginStatus.style.color = "red";
-            }
-            
-            // Re-enable the submit button immediately in case of failure
-            if (submitButton)
-                submitButton.disabled = false;
+          e.preventDefault();
+
+          const formData = new FormData(loginForm);
+
+          for (const [key, value] of formData.entries()) {
+              if (value.length > 50) {
+                  loginStatus.textContent = `${key} cannot exceed 50 characters.`;
+                  loginStatus.style.color = "red";
+                  return;
+              }
           }
-        } catch (error) {
-          console.error("Error logging in:", error);
-          loginStatus.textContent = "Error logging in. Please try again later.";
-          loginStatus.style.color = "red";
-  
-          // Re-enable the submit button immediately in case of error
+
           if (submitButton)
-            submitButton.disabled = false;
-        }
-  
-        return false;
+              submitButton.disabled = true;
+          let csrfToken = await getCSRFCookie();
+          try {
+              const response = await fetch(`${getBackendURL()}/login/`, {
+                  method: "POST",
+                  body: formData,
+                  headers: {
+                      "X-CSRFToken": csrfToken,
+                  },
+              });
+
+              const data = await response.json();
+
+              if ('message' in data && data.message === "Login successful") {
+                  localStorage.setItem("isLoggedIn", "true");
+                  if ('jwt_token' in data) {
+                      localStorage.setItem("jwtToken", data.jwt_token);
+                  }
+                  if ('userLogin' in data) {
+                      localStorage.setItem("userLogin", data.userLogin);
+                  }
+
+                  setTimeout(() => {
+                      if (submitButton)
+                          submitButton.disabled = false;
+                      window.location.href = "/";
+                  }, 1000);
+
+              } else {
+
+                  if (data.hasOwnProperty('error')) {
+                      loginStatus.textContent = data.error;
+                      loginStatus.style.color = "red";
+                  } else {
+                      loginStatus.textContent = "Login failed. Please check your credentials.";
+                      loginStatus.style.color = "red";
+                  }
+
+                  if (submitButton)
+                      submitButton.disabled = false;
+              }
+          } catch (error) {
+              console.error("Error logging in:", error);
+              loginStatus.textContent = "Error logging in. Please try again later.";
+              loginStatus.style.color = "red";
+
+              if (submitButton)
+                  submitButton.disabled = false;
+          }
+
+          return false;
       };
-  
+
       loginForm.addEventListener("submit", handleSubmit);
-    } else {
+  } else {
       console.error("Login form not found.");
-    }
   }
-  
+}
--- srcs/frontend/js/Register.js
+++ srcs/frontend/js/Register.js
@@ -1,6 +1,7 @@
 async function handleRegister() {
-const jwtToken = localStorage.getItem('jwtToken');
-const csrfToken = await getCSRFCookie(); 
+    const jwtToken = localStorage.getItem('jwtToken');
+    const csrfToken = await getCSRFCookie(); 
+
     Promise.all([
         translateKey('email'),
         translateKey('username'),
@@ -21,14 +22,18 @@ const csrfToken = await getCSRFCookie();
 
         document.getElementById("register-form").addEventListener("submit", function(event) {
             event.preventDefault(); 
-    
-            
+
             const email = document.getElementById("enterEmail").value;
             const username = document.getElementById("enterUsername").value;
             const password = document.getElementById("enterPassword").value;
             const confirmPassword = document.getElementById("confirmPasswordP").value;
             
-            
+            // Check if inputs exceed the maximum allowed length
+            if (username.length > 50 || email.length > 50 || password.length > 50 || confirmPassword.length > 50) {
+                alert('One or more input values exceed the maximum allowed length of 50 characters.');
+                return;
+            }
+
             if (password !== confirmPassword) {
                 document.getElementById("password-mismatch-feedback").style.display = "block";
                 document.getElementById("password-mismatch-feedback").textContent = "Passwords do not match.";
@@ -36,40 +41,36 @@ const csrfToken = await getCSRFCookie();
             } else {
                 document.getElementById("password-mismatch-feedback").style.display = "none";
             }
-    
 
             const formData = new FormData();
-formData.append("email", email);
-formData.append("username", username);
-formData.append("password", password);
-formData.append("confirm_password", confirmPassword);
+            formData.append("email", email);
+            formData.append("username", username);
+            formData.append("password", password);
+            formData.append("confirm_password", confirmPassword);
 
-
-                    
-fetch(`${getBackendURL()}/register/`, {
-    method: "POST",
-    headers: {
-        'Authorization': `Bearer ${jwtToken}`,
-        'X-CSRFToken': csrfToken
-    },
-    body: formData,
-})
-.then(response => {
-    return response.json();
-})
-.then(data => {
-    if (data.hasOwnProperty('error')) {
-    document.getElementById("register-status").textContent = data.error;
-    document.getElementById("register-status").style.color = "red";
- 
-} else if (data.hasOwnProperty('message')) {
-            document.getElementById("register-status").textContent = data.message;  
-            document.getElementById("register-status").style.color = "green";
-            setTimeout(() => {
-            window.location.href = "/#login";
-            }, 2000);
-        }  
-})
+            fetch(`${getBackendURL()}/register/`, {
+                method: "POST",
+                headers: {
+                    'Authorization': `Bearer ${jwtToken}`,
+                    'X-CSRFToken': csrfToken
+                },
+                body: formData,
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                if (data.hasOwnProperty('error')) {
+                    document.getElementById("register-status").textContent = data.error;
+                    document.getElementById("register-status").style.color = "red";
+                } else if (data.hasOwnProperty('message')) {
+                    document.getElementById("register-status").textContent = data.message;  
+                    document.getElementById("register-status").style.color = "green";
+                    setTimeout(() => {
+                        window.location.href = "/#login";
+                    }, 2000);
+                }  
+            })
             .catch(error => {
                 console.error("Error registering:", error);
                 document.getElementById("register-status").textContent = "Error registering "+error;
